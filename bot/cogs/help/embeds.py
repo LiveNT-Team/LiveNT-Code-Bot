9eea5a79e6e36a1b5f06d110e334798585fd6cd1
psevdonimux <mail_451@mail.ru>
2025-09-16 20:14:23 +0300

    Обновил описание /help

--- bot/cogs/help/embeds.py
+++ bot/cogs/help/embeds.py
@@ -9,29 +9,29 @@ class HelpForSectionEmbed(InfoEmbed):
             case HelpMenuSectionsEnum.AI:
                 self.add_field(
                     "ИИ Функционал",
-                    "/enable ai - включить ИИ функционал\n"
-                    "/disable ai - отключить ИИ функционал\n"
-                    "/ask - Запрос к ИИ также можно через @ без выбора личности")
+                    "</enable ai:0> - включить ИИ функционал\n"
+                    "</disable ai:0> - отключить ИИ функционал\n"
+                    "</ask:0> - Запрос к ИИ также можно через @ без выбора личности")
             case HelpMenuSectionsEnum.SETTINGS:
                 self.add_field(
                     "Настройка сервера",
-                    "/settings - Вывести информацию настроек сервера\n"
-                    "/set - Установить канал для общения с ИИ\n"
-                    "/set - Установить канал для приветствия от ИИ при входе\n"
-                    "/disable greetings - отключить приветствие от ИИ при входе\n"
-                    "/enable greetings - включить приветствие от ИИ при входе",
+                    "</settings:0> - Вывести информацию настроек сервера\n"
+                    "</set:0> - Установить канал для общения с ИИ\n"
+                    "</set:0> - Установить канал для приветствия от ИИ при входе\n"
+                    "</disable greetings:0> - отключить приветствие от ИИ при входе\n"
+                    "</enable greetings:0> - включить приветствие от ИИ при входе",
                 )
             case HelpMenuSectionsEnum.PERSONALITIES:
                 self.add_field("Личности ИИ", 
-                    "/change personality - Выбрать личность ИИ\n"
-                    "/get personalities - Список личностей ИИ")
+                    "</change personality:0> - Выбрать личность ИИ\n"
+                    "</get personalities:0> - Список личностей ИИ")
             case HelpMenuSectionsEnum.STATS:
                 self.add_field("Статистика", 
-                    "/stats - Статистика ввода сообщений\n"
-                    "/enable extraditing_activist - Включить выдачу роли Активист\n"
-                    "/disable extraditing_activist - Отключить выдачу роли Активист\n"
-                    "/set activist_messages_count - Установить порог для получения роли Активист\n"
-                    "/set activist_role - Установить роль Активист")
+                    "</stats:0> - Статистика ввода сообщений\n"
+                    "</enable extraditing_activist:0> - Включить выдачу роли Активист\n"
+                    "</disable extraditing_activist:0> - Отключить выдачу роли Активист\n"
+                    "</set activist_messages_count:0> - Установить порог для получения роли Активист\n"
+                    "</set activist_role:0> - Установить роль Активист")
 
 
 class HelpEmbed(InfoEmbed):
