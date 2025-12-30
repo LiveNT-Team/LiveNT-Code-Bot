c09557f35114db294363469c98669917a8078d60
theaihopgg <theaihopgg@gmail.com>
2025-12-30 07:12:03 +0700

    Улучшение скорости выполнения

--- bot/prestart.sh
+++ bot/prestart.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 if [ $IS_DEV_MODE -eq "0"]; then
     echo Applying migrations in product mode
-    yoyo apply --database mysql://$MYSQL_USERNAME:$MYSQL_PASSWORD@$MYSQL_HOST:3306/$MYSQL_DATABASE
+    yoyo apply --database mysql://$MYSQL_USERNAME:$MYSQL_PASSWORD@$MYSQL_HOST:3306/$MYSQL_DATABASE --batch
 else
     echo Applying migrations in dev mode
-    yoyo apply --database mysql://$DEV_MYSQL_USERNAME:$DEV_MYSQL_PASSWORD@$DEV_MYSQL_HOST:3306/$DEV_MYSQL_DATABASE
+    yoyo apply --database mysql://$DEV_MYSQL_USERNAME:$DEV_MYSQL_PASSWORD@$DEV_MYSQL_HOST:3306/$DEV_MYSQL_DATABASE --batch
 fi
 echo Migrations applied
 exec "$@"
