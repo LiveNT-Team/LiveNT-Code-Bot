ffaf92f5cff9faa89a180014c7e763586b30ff0c
theaihopgg <theaihopgg@gmail.com>
2026-01-04 18:59:56 +0700

    Добавлено описание команд

--- bot/cogs/settings/cog.py
+++ bot/cogs/settings/cog.py
@@ -20,7 +20,7 @@ class SettingsCog(Cog):
     async def get(self, inter: AppCmdInter) -> None:
         pass
 
-    @get.sub_command("settings")
+    @get.sub_command("settings", "Выводит настройки бота для этого сервера")
     async def get_settings(self, inter: AppCmdInter) -> None:
         if not inter.author.guild_permissions.administrator:
             return await inter.response.send_message(embed=NotEnoughPermissionsEmbed())
@@ -34,11 +34,14 @@ class SettingsCog(Cog):
         settings_embed.add_field("Тестовая опция", str(guild.test_option))
         await inter.response.send_message(embed=settings_embed, ephemeral=True)
 
-    @set.sub_command("test_option")
+    @set.sub_command("test_option", "Изменение значения опции")
     async def set_test_option(
         self,
         inter: AppCmdInter,
-        new_value: int,
+        new_value: int = Param(
+            1,
+            description="Новое значение для параметра. Если пустое - значение по умолчанию",
+        ),
     ) -> None:
         if not inter.author.guild_permissions.administrator:
             return await inter.response.send_message(embed=NotEnoughPermissionsEmbed())
