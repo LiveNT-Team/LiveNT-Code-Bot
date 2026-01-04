7d55df9c5513e02e7186b4e92940d138cfe36e3d
theaihopgg <theaihopgg@gmail.com>
2026-01-04 18:45:00 +0700

    Добавлен guilds сервис

---
+++ bot/services/guilds/service.py
@@ -0,0 +1,24 @@
+from typing import Literal
+from core.models.guild import Guild
+from services.mysqliup import MySqliUp
+
+
+async def get_guild_by_discord_gid(db: MySqliUp, gid: int) -> Guild | None:
+    if row := await db.select_row(
+        "guilds",
+        Guild._fields,
+        where="discord_gid = %s",
+        params=(gid,),
+    ):
+        return Guild._make(row)
+    return None
+
+
+async def get_or_create_by_discord_id(db: MySqliUp, gid: int) -> Guild:
+    if guild := await get_guild_by_discord_gid(db, gid):
+        return guild
+    await db.create_row("guilds", {"discord_gid": gid})
+    if guild := await get_guild_by_discord_gid(db, gid):
+        return guild
+    else:
+        raise ValueError("theaihopgg что за х***я")
